9fcfec247fc3abd090b25f1fe14b4de49156b00b
gilmariosoftware@gmail.com <gilmariosoftware@gmail.com>
2019-02-27 22:27:01 -0300

    Correções na arquitetura

---
+++ src/main/java/br/com/rapidscrum/rapidscrum/core/exception/HandleException.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.rapidscrum.rapidscrum.core.exception;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+/**
+ *
+ * @author gilmario
+ */
+@RestControllerAdvice
+public class HandleException extends ResponseEntityExceptionHandler{
+
+    
+   
+}
