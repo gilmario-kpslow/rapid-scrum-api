e125dabadfba873f6e6be4ec4ff1949910f8bea9
008647C6 - Gilmario batista <gilmario.costa@sefaz.ce.goc.br>
2019-07-29 17:00:02 -0300

    Atualizando bibliotecas para java 12

--- src/main/java/br/com/rapidscrum/rapidscrum/RapidscrumApplication.java
+++ src/main/java/br/com/rapidscrum/rapidscrum/RapidscrumApplication.java
@@ -1,5 +1,9 @@
 package br.com.rapidscrum.rapidscrum;
 
+import java.io.IOException;
+import org.hsqldb.Server;
+import org.hsqldb.persist.HsqlProperties;
+import org.hsqldb.server.ServerAcl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +13,20 @@ public class RapidscrumApplication {
     public static void main(String[] args) {
         System.setProperty("spring.devtools.restart.enabled", "true");
         SpringApplication.run(RapidscrumApplication.class, args);
+    }
+
+    private void iniciandoDb() throws IOException, ServerAcl.AclFormatException {
+        HsqlProperties p = new HsqlProperties();
+        p.setProperty("server.database.0", "file:rapiddb");
+        p.setProperty("server.dbname.0", "rapiddb");
+        // set up the rest of properties
+
+        // alternative to the above is
+        Server server = new Server();
+        server.setProperties(p);
+        server.setLogWriter(null); // can use custom writer
+        server.setErrWriter(null); // can use custom writer
+        server.start();
     }
 
 }
---
+++ src/main/java/br/com/rapidscrum/rapidscrum/projeto/Projeto.java
@@ -0,0 +1,5 @@
+package br.com.rapidscrum.rapidscrum.projeto;
+
+public class Projeto {
+
+}
